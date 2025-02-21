9e5d90c4e862b5ad3a9393371399217b1e3cee3e
Kazakova Anastasiia <130105.kaz@gmail.com>
2025-02-22 02:29:15 +0500

    Добавила исходный код ASP.NET проекта

---
+++ ASPNET/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.MapGet("/hello", () =>
+{
+    var mess = new[]
+    {
+        "Hello, World!",
+        "Привет, мир!",
+        "Сәлем, Әлем!",
+        "Hola, Mundo!",
+        "Bonjour, le monde!",
+        "Hallo, Welt!",
+        "こんにちは、世界！",
+        "Ciao, mondo!",
+        "안녕하세요, 세계!"
+    };
+    var random = new Random();
+    return Results.Json(new { mess = mess[random.Next(mess.Length)] });
+});
+
+app.MapGet("/time", () => Results.Json(new { time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") }));
+
+app.Run();
